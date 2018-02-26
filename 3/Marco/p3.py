5fa0b41b9cb46601e6c0622581ec77438a85a271
marco <marcoboncoraglio@gmail.com>
2018-02-27 00:04:51 +0100

    p3 nearly done

--- 3/Marco/p3.py
+++ 3/Marco/p3.py
@@ -1,38 +1,131 @@
-import matplotlib.pyplot as plt
-
 from sklearn.linear_model import perceptron
-from pandas import *
-from sklearn.metrics import accuracy_score
+import pandas as pd
+from sklearn.metrics import confusion_matrix
 
-inputs = pandas.read_csv('./phishing_websites.txt')
-inputs_train = inputs[:-3316]
-inputs_test = inputs[-3316:]
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+"""
+@attribute having_IP_Address  { -1,1 }
+@attribute URL_Length   { 1,0,-1 }
+@attribute Shortining_Service { 1,-1 }
+@attribute having_At_Symbol   { 1,-1 }
+@attribute double_slash_redirecting { -1,1 }
+@attribute Prefix_Suffix  { -1,1 }
+@attribute having_Sub_Domain  { -1,0,1 }
+@attribute SSLfinal_State  { -1,1,0 }
+@attribute Domain_registeration_length { -1,1 }
+@attribute Favicon { 1,-1 }
+@attribute port { 1,-1 }
+@attribute HTTPS_token { -1,1 }
+@attribute Request_URL  { 1,-1 }
+@attribute URL_of_Anchor { -1,0,1 }
+@attribute Links_in_tags { 1,-1,0 }
+@attribute SFH  { -1,1,0 }
+@attribute Submitting_to_email { -1,1 }
+@attribute Abnormal_URL { -1,1 }
+@attribute Redirect  { 0,1 }
+@attribute on_mouseover  { 1,-1 }
+@attribute RightClick  { 1,-1 }
+@attribute popUpWidnow  { 1,-1 }
+@attribute Iframe { 1,-1 }
+@attribute age_of_domain  { -1,1 }
+@attribute DNSRecord   { -1,1 }
+@attribute web_traffic  { -1,0,1 }
+@attribute Page_Rank { -1,1 }
+@attribute Google_Index { 1,-1 }
+@attribute Links_pointing_to_page { 1,0,-1 }
+@attribute Statistical_report { -1,1 }
+@attribute Result  { -1,1 }
+"""
+
+df = pd.read_csv("phishing_websites.txt")
+# print(df.head())
+y = df['Result']
+x = df.drop(['Result'], axis=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=27)
+
+# TODO: create 2d graph alpha by accuracy?
 # Create the perceptron object (net)
-net = perceptron.Perceptron(max_iter=1000, verbose=0, random_state=None, fit_intercept=True, eta0=0.002)
+net = perceptron.Perceptron(max_iter=100, verbose=0, random_state=None, fit_intercept=True, eta0=0.002)
 
 # Train the perceptron object (net)
-features = ['having_IP_Address', 'URL_Length', 'Shortining_Service', 'having_At_Symbol', 'double_slash_redirecting',
-             'Prefix_Suffix', 'having_Sub_Domain', 'SSLfinal_State', 'Domain_registeration_length', 'Favicon', 'port',
-             'HTTPS_token', 'Request_URL', 'URL_of_Anchor', 'Links_in_tags', 'SFH', 'Submitting_to_email',
-             'Abnormal_URL', 'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow', 'Iframe', 'age_of_domain',
-             'DNSRecord', 'web_traffic', 'Page_Rank', 'Google_Index', 'Links_pointing_to_page', 'Statistical_report']
+net.fit(x_train, y_train)
 
-net.fit(inputs_train[features], inputs_train['Result'])
-
-print(len(features))
 # Output the coefficints
-for i in range(len(features)):
+for i in range(len(x.columns)):
     print("Coefficient " + str(i+1) + ": " + str(net.coef_[0,i]))
-
 print("Bias " + str(net.intercept_))
 
 # Do a prediction
+pred = net.predict(x_test)
+acc = accuracy_score(y_test, pred) * 100
+print("accuracy: {0:2f}%".format(acc))
 
-inputs_test_list = inputs_test[features]
+# TODO: print confusion matrix using seaborn?
+#confusion_matrix(pred, df['Result'])
 
-pred = net.predict(inputs_test_list)
+# MLPClassifier
+print("\nMLPClassifier: ")
+y = df['Result']
+x = df.drop(['Result'], axis=1)
 
-# Confusion Matrix
-# confusion_matrix(pred, inputs['Result'])
-print("accuracy: {0:2f}%".format(accuracy_score(inputs_test[['Result']], pred) *100))
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=27)
+
+learning_rates = ['constant', 'invscaling', 'adaptive']
+alphas = [0.0001, 0.001, 0.01]
+optimizers = ['adam', 'lbfgs', 'sgd']
+alpha_list=[]
+acc_list=[]
+
+for opt in optimizers:
+    print("optimizer: ", opt)
+    for lr in range(len(learning_rates)):
+        print("learning rate:", learning_rates[lr])
+        for alpha in alphas:
+            print("alpha: ", alpha)
+            mlp = MLPClassifier(max_iter=50, alpha=alpha,
+                                learning_rate=learning_rates[lr], solver=opt, tol=0.00001)
+            mlp.fit(x_train, y_train)
+            y_pred = mlp.predict(x_test)
+            acc_score = accuracy_score(y_test, y_pred)
+            acc_list.append(acc_score)
+            alpha_list.append(alpha)
+
+            # TODO: print classification report?
+            # print(classification_report(y, y_pred))
+
+            print("Accuracy Score: ", acc_score)
+
+# build 3d plot loss(learning_rates, alphas)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_title(optimizers[0])
+ax.set_xlabel("alpha")
+ax.set_ylabel("0=constant, 1=invscaling, 2=adaptive")
+ax.set_zlabel("acc %")
+ax.plot(alpha_list[:3], range(3), acc_list[:3])
+plt.show()
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_title(optimizers[1])
+ax.set_xlabel("alpha")
+ax.set_ylabel("0=constant, 1=invscaling, 2=adaptive")
+ax.set_zlabel("acc %")
+ax.plot(alpha_list[3:6], range(3), acc_list[3:6])
+plt.show()
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_xlabel("alpha")
+ax.set_ylabel("0=constant, 1=invscaling, 2=adaptive")
+ax.set_zlabel("acc %")
+ax.set_title(optimizers[2])
+ax.plot(alpha_list[6:9], range(3), acc_list[6:9])
+plt.show()
